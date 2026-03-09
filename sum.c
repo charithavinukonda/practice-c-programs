b01d690896996ef04a6d16c2b854368e4c2187f0
charithavinukonda <charithavinukonda385@gmail.com>
2026-03-09 17:11:52 +0000

    added sum program

---
+++ sum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int main()
+{
+  int a,b;
+  printf("\nenter values of 2 numbers");
+  scanf("%d %d",&a,&b);
+  printf("before swap: a=%d\n,b=%d\n",a,b);
+  a=a+b;
+  b=a-b;
+  a=a-b;
+  printf("after swap a=%d\n,b=%d\n",a,b);
+  return 0;
+} 
